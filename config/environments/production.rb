991992b6a1c72198e1c624cada476ba1909213a4
sivagollapalli <siva@joshsoftware.com>
2012-12-11 16:46:02 +0000

    Updated production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Goldberg::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+   config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
@@ -29,13 +29,10 @@ Goldberg::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true 
+  #config.serve_static_assets = true 
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Disable delivery errors, bad email addresses will be ignored
-   config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
@@ -46,5 +43,5 @@ Goldberg::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => "ci.joshsoftware.com" }
+  config.action_mailer.raise_delivery_errors = true
 end
